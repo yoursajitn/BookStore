18113aa4b0fa7c17722d00640716e816ad37a965
Ajit Nimbalkar <yoursajitn@gmail.com>
2023-06-27 18:25:37 +0530

    Worked and Added DbContext Files

---
+++ MyBookShop/MyBookShop/Data/BookStoreContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MyBookShop.Data
+{
+    public class BookStoreContext:DbContext
+    {
+        public BookStoreContext(DbContextOptions<BookStoreContext> options):base(options)
+        {
+
+        }
+        public DbSet<Books> Books { get; set; }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlServer("Server=.;Database=BookStore;Integrated security=True;");
+        //    base.OnConfiguring(optionsBuilder);
+        //}
+    }
+}
---
+++ MyBookShop/MyBookShop/Data/Books.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyBookShop.Data
+{
+    public class Books
+    {
+        public int id { get; set; }
+        public string BookName { get; set; }
+        public string Author { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public string Language { get; set; }
+        public int TotalPages { get; set; }
+    }
+}
