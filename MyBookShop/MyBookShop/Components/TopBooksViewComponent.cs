3c4be151c6797ca34c609de992bfcda5e9b7e853
Ajit Nimbalkar <yoursajitn@gmail.com>
2023-07-30 19:15:52 +0530

    30-7-2023

---
+++ MyBookShop/MyBookShop/Components/TopBooksViewComponent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyBookShop.Components
+{
+    public class TopBooksViewComponent:ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
